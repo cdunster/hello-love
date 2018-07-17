e6690f30b8c1bb75771ff78bf2b055f2478de9b1
Callum Dunster <calonthexbox@hotmail.com>
2018-07-17 23:38:29 +0200

    Simple movement
    * Arrow keys move the rectangle.
    * No collision on edges.
    * No multi-key support.
    * Long if-else statement.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# hello-love
 An introduction into using the LÖVE (love2d.org) framework.
+
+# Current Implementation
+Use the arrow keys to move the rectangle around the screen. No collision on edges, no multi-press.
--- main.lua
+++ main.lua
@@ -1,3 +1,23 @@
+function love.load()
+    x = 100
+    y = 100
+end
+
+function love.update(dt)
+
+    step = 50 * dt
+
+    if love.keyboard.isDown("right") then
+        x = x + step
+    elseif love.keyboard.isDown("left") then
+        x = x - step
+    elseif love.keyboard.isDown("up") then
+        y = y - step
+    elseif love.keyboard.isDown("down") then
+        y = y + step
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello, Love!", 100, 100)
+    love.graphics.rectangle("line", x, y, 200, 150)
 end
