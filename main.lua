1c40d47b7365d191d16d26c28d109ae254fbd305
Callum Dunster <calonthexbox@hotmail.com>
2018-07-24 23:07:15 +0200

    Create enemy and move location of player.

--- main.lua
+++ main.lua
@@ -4,14 +4,18 @@ function love.load()
     Object = require("classic")
     require("character")
     require("player")
+    require("enemy")
 
-    player = Player(300, 20, 500, "resources/panda.png")
+    player = Player(350, 500, 500, "resources/panda.png")
+    enemy = Enemy(0, 20, 500, "resources/snake.png")
 end
 
 function love.update(dt)
     player:update(dt)
+    enemy:update(dt)
 end
 
 function love.draw()
     player:draw()
+    enemy:draw()
 end
