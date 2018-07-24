cee632601d81ababa5aefe370fb73aad382ab995
Callum Dunster <calonthexbox@hotmail.com>
2018-07-24 22:46:13 +0200

    Create a simple player.

--- main.lua
+++ main.lua
@@ -1,10 +1,17 @@
 --! file: The entry point of the game.
 
 function love.load()
+    Object = require("classic")
+    require("character")
+    require("player")
+
+    player = Player(300, 20, 500, "resources/panda.png")
 end
 
-function love.update(dt)    
+function love.update(dt)
+    player:update(dt)
 end
 
 function love.draw()
+    player:draw()
 end
