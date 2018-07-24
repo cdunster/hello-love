cb6d12d616a12e3eeac94303e837ea1b3220dfb5
Callum Dunster <calonthexbox@hotmail.com>
2018-07-24 22:28:30 +0200

    Wipe the slate clean.

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 An introduction into using the LÖVE (love2d.org) framework.
 
 # Current Implementation
-* After `2 seconds` a random enemy (red) will appear.
-* `Space` key creates a new rectangle (blue).
-* `Arrow keys` move the `blue rectangle(s)` around the screen (only one arrow key at once is registered).
-* Any time one or more `blue rectangle(s)` collide with the `red enemy` all collided shapes (including enemy) will be filled in. The un-collided shapes will remain un-filled.
+* Empty slate time. Check the revision history work working code.
--- main.lua
+++ main.lua
@@ -1,80 +1,10 @@
 --! file: The entry point of the game.
 
 function love.load()
-    tick = require("tick")
-    Object = require("classic")
-    require("shape")
-    require("rectangle")
-
-    shapes = {}
-
-    sheepSprite = love.graphics.newImage("resources/sheep.png")
-
-    drawEnemy = false
-    tick.delay(function() drawEnemy = true end, 2)
 end
 
-function love.keypressed(key)
-    if "space" == key then
-        table.insert(shapes, Rectangle(100, 100, 150, 50, "blue"))
-    end
-end
-
-function love.update(dt)
-    tick.update(dt)
-
-    if drawEnemy then
-        local x = math.random(0, 500)
-        local y = math.random(0, 500)
-        local size = math.random(50, 200)
-        enemy = Rectangle(x, y, size, size, "red")
-        drawEnemy = false
-    end
-
-    for i, shape in ipairs(shapes) do
-        shape:update(dt)
-    end
-    
+function love.update(dt)    
 end
 
 function love.draw()
-    love.graphics.reset()
-    love.graphics.draw(sheepSprite, 0, 500)
-
-    for i, shape in ipairs(shapes) do
-        local mode
-        if nil == enemy then
-            mode = "line"
-        elseif isCollidedWithEnemy(shape) then
-            mode = "fill"
-        else
-            mode = "line"
-        end
-
-        shape:draw(mode)
-        if nil ~= enemy then
-            enemy:draw(mode)
-        end
-    end
-end
-
-function isCollidedWithEnemy(shape)
-    local shape_left = shape.x
-    local shape_right = shape.x + shape.width
-    local shape_top = shape.y
-    local shape_bottom = shape.y + shape.height
-
-    local enemy_left = enemy.x
-    local enemy_right = enemy.x + enemy.width
-    local enemy_top = enemy.y
-    local enemy_bottom = enemy.y + enemy.height
-
-    if shape_right > enemy_left and
-    shape_left < enemy_right and
-    shape_bottom > enemy_top and
-    shape_top < enemy_bottom then
-        return true
-    else
-        return false
-    end
 end
