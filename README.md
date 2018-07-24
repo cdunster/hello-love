3acf0cce948cd8986beec3cd0fd81dc7dfdff449
Callum Dunster <calonthexbox@hotmail.com>
2018-07-24 22:22:14 +0200

    Add colour to rectangles. Use red Rectangle as enemy instead of Circle.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 An introduction into using the LÖVE (love2d.org) framework.
 
 # Current Implementation
-* After `2 seconds` a random circle will appear.
-* `Space` key creates a new rectangle.
-* `Arrow keys` move the rectangle(s) around the screen (only one arrow key at once is registered).
-* Any time one or more `rectangle` collides with the `circle` all collided shapes will be filled in. The un-collided shapes will remain un-filled.
+* After `2 seconds` a random enemy (red) will appear.
+* `Space` key creates a new rectangle (blue).
+* `Arrow keys` move the `blue rectangle(s)` around the screen (only one arrow key at once is registered).
+* Any time one or more `blue rectangle(s)` collide with the `red enemy` all collided shapes (including enemy) will be filled in. The un-collided shapes will remain un-filled.
--- main.lua
+++ main.lua
@@ -5,31 +5,30 @@ function love.load()
     Object = require("classic")
     require("shape")
     require("rectangle")
-    require("circle")
 
     shapes = {}
 
     sheepSprite = love.graphics.newImage("resources/sheep.png")
 
-    drawCircle = false
-    tick.delay(function() drawCircle = true end, 2)
+    drawEnemy = false
+    tick.delay(function() drawEnemy = true end, 2)
 end
 
 function love.keypressed(key)
     if "space" == key then
-        table.insert(shapes, Rectangle(100, 100, 150, 50))
+        table.insert(shapes, Rectangle(100, 100, 150, 50, "blue"))
     end
 end
 
 function love.update(dt)
     tick.update(dt)
 
-    if drawCircle then
+    if drawEnemy then
         local x = math.random(0, 500)
         local y = math.random(0, 500)
-        local radius = math.random(50, 200)
-        circle = Circle(x, y, radius)
-        drawCircle = false
+        local size = math.random(50, 200)
+        enemy = Rectangle(x, y, size, size, "red")
+        drawEnemy = false
     end
 
     for i, shape in ipairs(shapes) do
@@ -39,40 +38,41 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.reset()
     love.graphics.draw(sheepSprite, 0, 500)
 
     for i, shape in ipairs(shapes) do
         local mode
-        if nil == circle then
+        if nil == enemy then
             mode = "line"
-        elseif isCollidedWithCircle(shape) then
+        elseif isCollidedWithEnemy(shape) then
             mode = "fill"
         else
             mode = "line"
         end
 
         shape:draw(mode)
-        if nil ~= circle then
-            circle:draw(mode)
+        if nil ~= enemy then
+            enemy:draw(mode)
         end
     end
 end
 
-function isCollidedWithCircle(shape)
+function isCollidedWithEnemy(shape)
     local shape_left = shape.x
     local shape_right = shape.x + shape.width
     local shape_top = shape.y
     local shape_bottom = shape.y + shape.height
 
-    local circle_left = circle.x
-    local circle_right = circle.x + circle.radius
-    local circle_top = circle.y
-    local circle_bottom = circle.y + circle.radius
+    local enemy_left = enemy.x
+    local enemy_right = enemy.x + enemy.width
+    local enemy_top = enemy.y
+    local enemy_bottom = enemy.y + enemy.height
 
-    if shape_right > circle_left and
-    shape_left < circle_right and
-    shape_bottom > circle_top and
-    shape_top < circle_bottom then
+    if shape_right > enemy_left and
+    shape_left < enemy_right and
+    shape_bottom > enemy_top and
+    shape_top < enemy_bottom then
         return true
     else
         return false
