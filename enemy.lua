983c1b683656810af74968c3d931ef4abc435695
Callum Dunster <calonthexbox@hotmail.com>
2018-07-25 21:10:42 +0200

    Improve enemy direction logic.

--- enemy.lua
+++ enemy.lua
@@ -4,16 +4,18 @@ Enemy = Character:extend()
 
 function Enemy:new(x, y, speed, imageDir)
     Enemy.super.new(self, x, y, speed, imageDir)
+    self.directionMultiplier = 1
 end
 
 function Enemy:update(dt)
     local window_width = love.graphics.getWidth()
 
-    self.x = self.x + (self.speed * dt)
+    local directional_speed = self.speed * self.directionMultiplier
+    self.x = self.x + (directional_speed * dt)
 
     if 0 > self.x then
-        self.speed = -self.speed
+        self.directionMultiplier = 1
     elseif window_width < (self.x + self.width) then
-        self.speed = -self.speed
+        self.directionMultiplier = -1
     end
 end
