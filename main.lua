cb59c94a2bee1b651443471cf907ebb9b9a2e015
Callum Dunster <calonthexbox@hotmail.com>
2018-08-04 00:13:49 +0200

    Circle tracks mouse pointer.

--- main.lua
+++ main.lua
@@ -1,10 +1,33 @@
 --! file: The entry point of the game.
 
 function love.load()
+    circle = {}
+
+    circle.x = 100
+    circle.y = 100
+    circle.radius = 25
+    circle.speed = 200
 end
 
-function love.update(dt)    
+function love.update(dt)
+    mouse_x, mouse_y = love.mouse.getPosition()
+
+    angle = math.atan2(mouse_y - circle.y, mouse_x - circle.x)
+
+    cos = math.cos(angle)
+    sin = math.sin(angle)
+
+    circle.x = circle.x + circle.speed * cos * dt
+    circle.y = circle.y + circle.speed * sin * dt
 end
 
 function love.draw()
+    love.graphics.circle("line", circle.x, circle.y, circle.radius)
+
+    love.graphics.print("angle: " .. angle, 10, 10)
+
+    love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
+    love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
+
+    love.graphics.line(mouse_x, mouse_y, mouse_x, circle.y)
 end
